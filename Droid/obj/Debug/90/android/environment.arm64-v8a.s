345368565e7aff4a4874713a844cdaace333832f
a.abdiukov <a.abdiukov@gmail.com>
2021-01-05 17:42:27 +1100

    upload(2)

--- Droid/obj/Debug/90/android/environment.arm64-v8a.s
+++ Droid/obj/Debug/90/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"78fba00b-9ee9-4484-b13f-3e0123625adb"
+	.asciz	"d389f3df-10c3-4445-a42c-889f4067793a"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
